8d8a3a916c77a9cfa7c21f20e4c460ee3f4d85c6
Konstantin Surikov <surikov@gmail.com>
2020-02-04 12:34:04 +0300

    Учебный проект: консольные забавы

---
+++ src/service/cli/generate.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const fs = require(`fs`);
+
+const {getRandomInt, shuffle} = require(`../../utils`);
+
+const DEFAULT_COUNT = 1;
+const MAX_COUNT = 1000;
+const MAX_CATEGORIES = 3;
+const FILE_NAME = `mocks.json`;
+
+const TITLES = [
+  `Продам книги Стивена Кинга.`,
+  `Продам новую приставку Sony Playstation 5.`,
+  `Продам отличную подборку фильмов на VHS.`,
+  `Куплю антиквариат.`,
+  `Куплю породистого кота.`,
+  `Продам коллекцию журналов «Огонёк».`,
+  `Отдам в хорошие руки подшивку «Мурзилка».`,
+  `Продам советскую посуду. Почти не разбита.`,
+  `Куплю детские санки.`,
+];
+
+const SENTENCES = [
+  `Товар в отличном состоянии.`,
+  `Пользовались бережно и только по большим праздникам.,`,
+  `Продаю с болью в сердце...`,
+  `Бонусом отдам все аксессуары.`,
+  `Даю недельную гарантию.`,
+  `Если товар не понравится — верну всё до последней копейки.`,
+  `Это настоящая находка для коллекционера!`,
+  `Если найдёте дешевле — сброшу цену.`,
+  `Таких предложений больше нет!`,
+  `Две страницы заляпаны свежим кофе.`,
+  `При покупке с меня бесплатная доставка в черте города.`,
+  `Кажется, что это хрупкая вещь.`,
+  `Мой дед не мог её сломать.`,
+  `Кому нужен этот новый телефон, если тут такое...`,
+  `Не пытайтесь торговаться. Цену вещам я знаю.`,
+];
+
+const CATEGORIES = [
+  `Книги`,
+  `Разное`,
+  `Посуда`,
+  `Игры`,
+  `Животные`,
+  `Журналы`,
+];
+
+const OfferType = {
+  offer: `offer`,
+  sale: `sale`,
+};
+
+
+const SumRestrict = {
+  min: 1000,
+  max: 100000,
+};
+
+const PictureRestrict = {
+  min: 1,
+  max: 16
+};
+
+const getPictureFileName = (random) => {
+  return (`item${random < 10 ? `0` + random : random}.jpg`);
+};
+
+const getCategories = (count) => {
+  return shuffle(CATEGORIES).slice(0, count);
+};
+
+const generateOffers = (count) => (
+  Array(count).fill({}).map(() => ({
+    type: Object.keys(OfferType)[Math.floor(Math.random() * Object.keys(OfferType).length)],
+    title: TITLES[getRandomInt(0, TITLES.length - 1)],
+    description: shuffle(SENTENCES).slice(0, getRandomInt(1, 5)).join(` `),
+    sum: getRandomInt(SumRestrict.min, SumRestrict.max),
+    picture: getPictureFileName(getRandomInt(PictureRestrict.min, PictureRestrict.max)),
+    category: getCategories(getRandomInt(1, MAX_CATEGORIES)),
+  }))
+);
+
+module.exports = {
+  name: `--generate`,
+
+  run(args) {
+    const [count] = args;
+    const countOffer = Number.parseInt(count, 10) || DEFAULT_COUNT;
+    if (countOffer < MAX_COUNT) {
+      const content = JSON.stringify(generateOffers(countOffer));
+      fs.writeFile(FILE_NAME, content, (err) => {
+        if (err) {
+          console.error(`Can't write data to file...`);
+          process.exit(1);
+
+        }
+        return console.info(`Operation success. File created.`);
+      });
+    } else {
+      console.error(`Не больше 1000 объявлений`);
+      process.exit(1);
+    }
+
+  }
+};
