877b511b6504a093ed90f57937db3d5546350937
Konstantin Surikov <surikov@gmail.com>
2020-03-08 02:41:45 +0300

    Учебный проект: шаблонизируй это

--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -3,14 +3,9 @@
 const {Router} = require(`express`);
 const offersRouter = new Router();
 
-offersRouter.get(`/add`, (req, res) => res.send(`/offers/add`));
-offersRouter.get(`/:id`, (req, res) => {
-
-  res.send(`/offers/:id${req.params.id}`);
-});
-offersRouter.get(`/category/:id`,
-    (req, res) => res.send(`/offers/category/:id${req.params.id}`));
-offersRouter.get(`/edit/:id`,
-    (req, res) => res.send(`/offers/edit/:id${req.params.id}`));
+offersRouter.get(`/add`, (req, res) => res.render(`offers/add`));
+offersRouter.get(`/:id`, (req, res) => res.render(`offers/ticket`));
+offersRouter.get(`/category/:id`, (req, res) => res.render(`offers/category`));
+offersRouter.get(`/edit/:id`, (req, res) => res.render(`offers/edit`));
 
 module.exports = offersRouter;
