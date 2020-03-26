c152e513bc03aa1d340ccd52e682faedb887b7fd
Konstantin Surikov <surikov@gmail.com>
2020-03-27 01:16:59 +0300

    Refactored according to the textbook

--- src/service/cli/server.js
+++ src/service/cli/server.js
@@ -1,8 +1,31 @@
 'use strict';
 const express = require(`express`);
-const offersRouter = require(`./routes/offers`);
+const fs = require(`fs`).promises;
 
 const DEFAULT_PORT = 3000;
+const FILENAME = `mocks.json`;
+const HttpCode = {
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+};
+
+const app = express();
+app.use(express.json());
+
+app.get(`/offers`, async (req, res) => {
+  try {
+    const fileContent = await fs.readFile(FILENAME);
+    const mocks = JSON.parse(fileContent);
+    res.json(mocks);
+  } catch (err) {
+    res.status(HttpCode.INTERNAL_SERVER_ERROR).send(err);
+  }
+});
+
+app.use((req, res) => res
+  .status(HttpCode.NOT_FOUND)
+  .send(`Not found`));
+
 
 module.exports = {
   name: `--server`,
@@ -10,13 +33,11 @@ module.exports = {
     const [portArg] = args;
     const port = parseInt(portArg, 10) || DEFAULT_PORT;
 
-    const httpServer = express();
-
-    httpServer.use(express.json());
-
-    httpServer.use(`/offers`, offersRouter);
-
-    httpServer.listen(port, () => console.log(`Принимаю подключения на порт ${port}`));
+    app.listen(port, (err) => {
+      if (err) {
+        return console.error(`Ошибка при создании сервера`);
+      }
+      return console.log(`Принимаю подключения на порт ${port}`);
+    });
   }
-
 };
